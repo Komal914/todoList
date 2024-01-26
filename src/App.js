9d21ccfc9602720a7ead2d46a0e78ca2ec8a0bc3
Komal Kaur <komalkaurstaymenity@gmail.com>
2024-01-25 17:21:24 -0800

    header add, work mode button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import TodoWrapper from "./components/TodoWrapper";
+import Navigation from "./components/Navigation";
+import SidePanel from "./components/SidePanel";
 
 function App() {
   return (
     <div className="App">
+      <Navigation></Navigation>
       <TodoWrapper></TodoWrapper>
     </div>
   );
---
+++ src/components/SidePanel.js
@@ -0,0 +1,7 @@
+import WorkMode from "./WorkMode";
+
+const SidePanel = () => {
+  return <div className="SidePanel"></div>;
+};
+
+export default SidePanel;
