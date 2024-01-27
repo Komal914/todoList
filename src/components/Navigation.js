bfcaec525733840e4aa5294de6eae7a6dbadd3dc
Komal Kaur <komalkaurstaymenity@gmail.com>
2024-01-27 11:58:38 -0800

    created navigation, new logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,11 @@
 import WorkMode from "./WorkMode";
-import { ReactComponent as Coffee } from "../mug-hot-alt_6349588.svg";
+import logo from "../coffeeLogo.png";
 import { Fragment } from "react";
 
 const Navigation = () => {
   return (
     <Fragment>
-      <Coffee className="Logo"></Coffee>
+      <img src={logo} className="Logo" />
       <WorkMode></WorkMode>
     </Fragment>
   );
--- src/components/WorkMode.js
+++ src/components/WorkMode.js
@@ -1,15 +1,18 @@
 import { useState } from "react";
 
 const WorkMode = () => {
-  const [workMode, setWorkMode] = useState("false");
+  const [workMode, setWorkMode] = useState(false);
 
   const workModeHandler = () => {
     setWorkMode(!workMode);
   };
 
   return (
-    <div className="WorkModeButton" onClick={workModeHandler}>
-      Work Mode {workMode ? "on" : "off"}
+    <div
+      className={workMode ? "WorkOn WorkModeButton" : "WorkModeButton WorkOff"}
+      onClick={workModeHandler}
+    >
+      Work Mode {workMode ? "On" : "Off"}
     </div>
   );
 };
