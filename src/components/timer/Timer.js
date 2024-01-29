8673c0cffbec5775c58a66c88d5da672459c7226
Komal Kaur <komalkaurstaymenity@gmail.com>
2024-01-29 00:46:20 -0800

    sidePanel UI complete

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,11 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
 import "./timer.css";
 
 const Timer = () => {
   return (
-    <div className="Timer">
-      <h1>10</h1>
-      <h1>:</h1>
-      <h1>00</h1>
+    <div className="Timer-Container">
+      <div className="Timer">
+        <h1>10</h1>
+        <h1>:</h1>
+        <h1>00</h1>
+      </div>
+      <div className="TimerButtons">
+        <FontAwesomeIcon icon={faPlay} />
+        <FontAwesomeIcon icon={faMusic} />
+      </div>
     </div>
   );
 };
