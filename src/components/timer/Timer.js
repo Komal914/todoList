47760171ffd3e37ac68304bd5627f06df3cc18b2
Komal Kaur <komalkaurstaymenity@gmail.com>
2024-01-29 23:41:23 -0800

    dummy time working in timer

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,15 +1,36 @@
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faPause } from "@fortawesome/free-solid-svg-icons";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
+
 import "./timer.css";
 
 const Timer = () => {
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const deadline = "January, 30, 2024";
+
+  const getTime = () => {
+    const time = Date.parse(deadline) - Date.now();
+
+    setMinutes(Math.floor((time / 1000 / 60) % 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => getTime(deadline), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="Timer-Container">
       <div className="Timer">
-        <h1>10</h1>
+        <h1>{minutes}</h1>
         <h1>:</h1>
-        <h1>00</h1>
+        <h1>{seconds}</h1>
       </div>
       <div className="TimerButtons">
         <FontAwesomeIcon icon={faPlay} />
