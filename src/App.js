4ea043a503d6cebd4d8bcae7f8c87b7869963746
Komal Kaur <komalkaurstaymenity@gmail.com>
2024-02-06 14:12:53 -0800

    navbar mobile updated

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import ToDoContainer from "./components/TodoContainer/TodoContainer";
 import Navigation from "./components/navigation/Navigation";
 import LofiVideoPlayer from "./components/lofiVideoPlayer/LofiVideoPlayer";
 import "./App.css";
+import { MobileModeContextProvider } from "./context/MobileModeContext";
 
 function App() {
   return (
-    <WorkModeContextProvider>
-      <div className="App">
-        <Navigation></Navigation>
-        <ToDoContainer></ToDoContainer>
-        <LofiVideoPlayer></LofiVideoPlayer>
-      </div>
-    </WorkModeContextProvider>
+    <MobileModeContextProvider>
+      <WorkModeContextProvider>
+        <div className="App">
+          <Navigation></Navigation>
+          <ToDoContainer></ToDoContainer>
+          <LofiVideoPlayer></LofiVideoPlayer>
+        </div>
+      </WorkModeContextProvider>
+    </MobileModeContextProvider>
   );
 }
 
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -4,14 +4,20 @@ import WorkModeButton from "../WorkModeButton/WorkModeButton";
 import logo from "../../assets/coffeeLogo.png";
 import SidePanel from "../sidePanel/SidePanel";
 import "./navigation.css";
+import { MobileModeContext } from "../../context/MobileModeContext";
 
 const Navigation = () => {
   const { workMode } = useContext(WorkModeContext);
+  const { mobile } = useContext(MobileModeContext);
   return (
-    <div className="Navigation">
-      <img alt="coffee logo" src={logo} className="Logo"></img>
-      <WorkModeButton></WorkModeButton>
-      {workMode ? <SidePanel></SidePanel> : null}
+    <div className={!mobile ? "Navigation" : "NavigationMobile"}>
+      {!mobile ? <img alt="coffee logo" src={logo} className="Logo"></img> : ""}
+      <div
+        className={mobile ? "NavigationSidePanelMobile" : "NavigationSidePanel"}
+      >
+        <WorkModeButton></WorkModeButton>
+        {workMode ? <SidePanel></SidePanel> : null}
+      </div>
     </div>
   );
 };
